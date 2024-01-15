360bf386c7a51f4f2c96be68bb115e9f1d2daecd
mike <116024673+mikcad@users.noreply.github.com>
2024-01-15 17:02:50 +0000

    created variables for html elements
    made variables for all the current html elements within the html.index so that they can be referenced later.

---
+++ assets/js/logic.js
@@ -0,0 +1,22 @@
+// Global Variables
+// start screen variables
+const startContainer = document.getElementById("start-screen");
+const startButton = document.getElementById("start");
+
+// question screen variables
+const questionContainer = document.getElementById("questions");
+let questionTitle = document.getElementById("question-title");
+let questionChoices = document.getElementById("choices");
+
+// end screen variables
+const endContainer = document.getElementById("end-screen");
+let finalScore = document.getElementById("final-score");
+let playerInitials = document.getElementById("initials");
+const submitButton = document.getElementById("submit");
+
+// timer variables
+const timerContianer = document.getElementById("timer-container");
+let timerDisplay = document.getElementById("time");
+
+// highscore variables
+const highScore = document.getElementById("scores");
